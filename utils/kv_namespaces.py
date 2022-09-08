f96af5b009639c4eb2f3875ddf2182b035556e70
Ciel <me@cieldev.net>
2022-09-08 10:58:41 +0900

    Update kv_namespaces.py

--- utils/kv_namespaces.py
+++ utils/kv_namespaces.py
@@ -2,8 +2,8 @@ from workers_kv.ext import async_workers_kv
 from utils import envs
 
 guild_ranking_namespace = async_workers_kv.Namespace(account_id=envs.WORKERS_KV_ACCOUNT,
-                                                     namespace_id="envs.WORKERS_KV_NAMESPACE",
+                                                     namespace_idenvs.WORKERS_KV_NAMESPACE,
                                                      api_key=envs.CF_API_KEY)
 global_ranking_namespace = async_workers_kv.Namespace(account_id=envs.WORKERS_KV_ACCOUNT,
-                                                      namespace_id="envs.WORKERS_KV_NAMESPACE",
+                                                      namespace_id=envs.WORKERS_KV_NAMESPACE,
                                                       api_key=envs.CF_API_KEY)
