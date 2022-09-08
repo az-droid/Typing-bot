db83521b3e73fc57026d5bd02eca9bc5fbd4b206
Az <113041774+az-droid@users.noreply.github.com>
2022-09-08 10:41:13 +0900

    Update kv_namespaces.py
    #1 に対する修正

--- utils/kv_namespaces.py
+++ utils/kv_namespaces.py
@@ -2,8 +2,8 @@ from workers_kv.ext import async_workers_kv
 from utils import envs
 
 guild_ranking_namespace = async_workers_kv.Namespace(account_id=envs.WORKERS_KV_ACCOUNT,
-                                                     namespace_id="b221db35965344b1b0e0a3d2c53cf6b4",
+                                                     namespace_id=envs.WORKERS_KV_NAMESPACE,
                                                      api_key=envs.CF_API_KEY)
 global_ranking_namespace = async_workers_kv.Namespace(account_id=envs.WORKERS_KV_ACCOUNT,
-                                                      namespace_id="41f52d54a13c453f8175d44b6083b74e",
+                                                      namespace_id=envs.WORKERS_KV_NAMESPACE,
                                                       api_key=envs.CF_API_KEY)
